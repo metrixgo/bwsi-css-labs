414592c3f38883f6171ebb560983dad1a4fbe819
Zhendi Cao <metrixgo9@gmail.com>
2026-02-08 16:43:47 -0800

    tests

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -37,5 +37,8 @@ def test_invalid_operation():
     with pytest.raises(ValueError, match="Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'."):
         simple_calculator("", 5, 3)                     # Test for empty operation
 
+def test_funny():
+    assert simple_calculator("add", 6, 7) == 13
+
 if __name__ == "__main__":
     pytest.main()
